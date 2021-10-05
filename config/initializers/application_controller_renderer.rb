5771a2f075b6306f92e825ba889da1c7cab362b2
miotakikawa <m.n.baton1223@gmail.com>
2021-10-05 12:07:36 +0000

    [finish]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '90388e48ecf14685e2667f526cf0a70cbbbc22fafcae6d91a2d8c438e0da5df59d0d92c64b2bd89ebb89466586a9238526b01ec46bc206c7ccfb0ed46e7e58ac'
